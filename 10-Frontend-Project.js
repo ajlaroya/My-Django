db21a3ba106974bcfe99a1116310061391117b23
ajlaroya <arthur.j.laroya@gmail.com>
2021-09-02 21:24:09 +1000

    in a playable state, some funky fixes

--- 10-Frontend-Project.js
+++ 10-Frontend-Project.js
@@ -1,16 +1,14 @@
-// adds numbers to button as a visual reference/aid
-var slots = $("button");
-
-for (slot in slots) {
-  slots.eq(slot).text(slot);
+// adds numbers to buttons as a visual reference/aid
+for (slot in $("button")) {
+  $("button").eq(slot).text(slot);
 }
 
 // prompt for P1 and P2 name
-var p1 = prompt("Player 1: Please enter your name. You will be blue.");
-var p1Colour = '#ebc334'; // yellow
+var p1 = prompt("Player 1: Please enter your name. You will be red.");
+var p1Colour = '#eb4034';
 
 var p2 = prompt("Player 2: Please enter your name. You will be yellow.");
-var p2Colour = '#eb3434';
+var p2Colour = '#ffea00';
 
 // game conditions
 var game_on = true;
@@ -24,12 +22,12 @@ function logWin(rowNum,colNum){
 }
 
 // change colour of specific column and row index of a table
-function changeColor(rowIndex,colIndex,colour){
+function changeColour(rowIndex,colIndex,colour){
   return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color',colour);
 }
 
 // return colour for later use
-function returnColor(rowIndex,colIndex){
+function returnColour(rowIndex,colIndex){
   return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color');
 }
 
@@ -38,7 +36,7 @@ function getBottom(colIndex){
   var colourReport = returnColour(5, colIndex);
   for (var row = 5; row > -1; row--) {
     colourReport = returnColour(row,colIndex);
-    if (colourReport === 'white') {
+    if (colourReport === "rgb(255, 255, 255)") {
       return row;
     }
   }
@@ -46,8 +44,89 @@ function getBottom(colIndex){
 
 // connect four check
 function connectFour(one,two,three,four){
-  return (one == two && one == three && one == four && one !== 'white' & one !== undefined);
+  return (one == two && one == three && one == four && one !== "rgb(255, 255, 255)" & one !== undefined);
 }
+
+// Check for Horizontal Wins
+function horizontalWinCheck() {
+  for (var row = 0; row < 6; row++) {
+    for (var col = 0; col < 4; col++) {
+      if (connectFour(returnColour(row,col), returnColour(row,col+1) ,returnColour(row,col+2), returnColour(row,col+3))) {
+        console.log('horiz');
+        logWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
+}
+
+// Check for Vertical Wins
+function verticalWinCheck() {
+  for (var col = 0; col < 7; col++) {
+    for (var row = 0; row < 3; row++) {
+      if (connectFour(returnColour(row,col), returnColour(row+1,col) ,returnColour(row+2,col), returnColour(row+3,col))) {
+        console.log('vertical');
+        logWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
+}
+
+// Check for Diagonal Wins
+function diagonalWinCheck() {
+  for (var col = 0; col < 5; col++) {
+    for (var row = 0; row < 7; row++) {
+      if (connectFour(returnColour(row,col), returnColour(row+1,col+1) ,returnColour(row+2,col+2), returnColour(row+3,col+3))) {
+        console.log('diag');
+        logWin(row,col);
+        return true;
+      }else if (connectFour(returnColour(row,col), returnColour(row-1,col+1) ,returnColour(row-2,col+2), returnColour(row-3,col+3))) {
+        console.log('diag');
+        logWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
+}
+
+// starts with player 1
+var currentPlayer = 1;
+var currentName = p1;
+var currentColour = p1Colour;
+
+// game logic
+$('#status').text(p1+" it is your turn, pick a column to drop in!");
+
+$('.board button').on('click',function(){
+  var col = $(this).closest('td').index();
+  var bottom = getBottom(col);
+  changeColour(bottom, col, currentColour);
+
+  if (horizontalWinCheck() || verticalWinCheck() || diagonalWinCheck()) {
+    $('h1').text(currentName+" has won! Refresh browser to play again.");
+    // $('#instruct').fadeOut('fast'); slim jquery = no animations
+    // $('#status').fadeOut('fast');
+  }
+
+  currentPlayer = currentPlayer * -1 // no win or tie
+
+  if (currentPlayer === 1){
+    currentName = p1;
+    $('#status').text(currentName+" it is your turn.");
+    currentColour = p1Colour;
+  } else {
+    currentName = p2;
+    $('#status').text(currentName+" it is your turn.");
+    currentColour = p2Colour;
+  }
+})
 
 // REDUNDANT CODE BY YOURS TRULY:
 
