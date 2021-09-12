d05a7866c7e3605cdec6ee1fc48e72679f1bbd89
ajlaroya <arthur.j.laroya@gmail.com>
2021-09-12 11:28:42 +1000

    index and help page mapping

--- ProTwo/AppTwo/urls.py
+++ ProTwo/AppTwo/urls.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from AppTwo import views
 
 urlpatterns = [
-    re_path(r'^$',views.index,name='index'),
+    re_path(r'^$',views.help,name='help'),
 ]
--- ProTwo/AppTwo/views.py
+++ ProTwo/AppTwo/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    return HttpResponse("<em>My Second Project</em>")
+
+def help(request):
     my_dict = {'insert_help':"Help Page"}
     return render(request,'AppTwo/help.html',context=my_dict)
