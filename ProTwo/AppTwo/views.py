b417c790d75f4a03325d834f21d7e0302585ae03
ajlaroya <arthur.j.laroya@gmail.com>
2021-09-12 11:05:37 +1000

    templates practice adding help page

--- ProTwo/AppTwo/views.py
+++ ProTwo/AppTwo/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<em>Second Project</em")
+    my_dict = {'insert_help':"Help Page"}
+    return render(request,'AppTwo/help.html',context=my_dict)
--- ProTwo/ProTwo/urls.py
+++ ProTwo/ProTwo/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import re_path
+from django.urls import include
 from AppTwo import views
 
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
+    re_path(r'^help/',include('AppTwo.urls')),
     path('admin/', admin.site.urls),
 ]
