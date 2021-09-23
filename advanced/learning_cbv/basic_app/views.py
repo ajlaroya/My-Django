436d41f08ea1ae3994a671ae0cdc6ff673d78b17
ajlaroya <arthur.j.laroya@gmail.com>
2021-09-23 21:22:51 +1000

    started ListView and DetailView templates in app folder

--- advanced/learning_cbv/basic_app/views.py
+++ advanced/learning_cbv/basic_app/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView, DetailView
 from django.http import HttpResponse
+from . import models # . = look at CWD
 
-# Class based views!
+# CLASS BASED VIEWS /
 
 # class CBView(View):
 #     def get(self,request):
 #         return HttpResponse('Class based views are cool!')
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
+# class IndexView(TemplateView):
+#     template_name = 'index.html'
+#
+#     def get_context_data(self,**kwargs): # keyword arguments
+#         context = super().get_context_data(**kwargs)
+#         context['injectme'] = 'BASIC INJECTION'
+#         return context
 
-    def get_context_data(self,**kwargs): # keyword arguments
-        context = super().get_context_data(**kwargs)
-        context['injectme'] = 'BASIC INJECTION'
-        return context
+class SchoolListView(ListView):
+    context_object_name = 'schools'
+    model = models.School
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
