186f24347807be910f08ae4e9fe1c54ff4e432bf
ajlaroya <arthur.j.laroya@gmail.com>
2021-09-24 12:53:10 +1000

    CRUD (Create, read, update, delete) Views

--- advanced/learning_cbv/basic_app/views.py
+++ advanced/learning_cbv/basic_app/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.views.generic import (View, TemplateView,
+                                ListView, DetailView,
+                                CreateView, UpdateView,
+                                DeleteView)
 from django.http import HttpResponse
+from django.urls import reverse_lazy
 from . import models # . = look at CWD
 
-# CLASS BASED VIEWS /
+# CLASS BASED VIEWS
 
 # class CBView(View):
 #     def get(self,request):
 #         return HttpResponse('Class based views are cool!')
 
-# class IndexView(TemplateView):
-#     template_name = 'index.html'
-#
-#     def get_context_data(self,**kwargs): # keyword arguments
-#         context = super().get_context_data(**kwargs)
-#         context['injectme'] = 'BASIC INJECTION'
-#         return context
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    # def get_context_data(self,**kwargs): # keyword arguments
+    #     context = super().get_context_data(**kwargs)
+    #     context['injectme'] = 'BASIC INJECTION'
+    #     return context
 
 class SchoolListView(ListView):
     context_object_name = 'schools'
@@ -25,3 +29,15 @@ class SchoolDetailView(DetailView):
     context_object_name = 'school_detail'
     model = models.School
     template_name = 'basic_app/school_detail.html'
+
+class SchoolCreateView(CreateView):
+    fields = ('name','principal','location')
+    model = models.School
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name','principal')
+    model = models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy('basic_app:list') # wait until success
