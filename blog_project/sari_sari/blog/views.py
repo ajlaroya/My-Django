eb3e9e7da59ae37f82834331c920b952cfb680a9
ajlaroya <arthur.j.laroya@gmail.com>
2021-09-25 11:56:18 +1000

    creating class based views and connecting them to urls.py

--- blog_project/sari_sari/blog/views.py
+++ blog_project/sari_sari/blog/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
+from django.contrib.auth.mixins import LoginRequiredMixin # class based 'decorator'
+from django.urls import reverse_Lazy
+from blog.models import Post, Comment
+from blog.forms import PostForm, CommentForm
 
 # Create your views here.
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    # python sql query
+    def get_queryset(self):
+        # filter post model by descending (-) published date
+        # __lte = field lookup
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model = Post
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list') # redirect until after deletion
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        # query published dates with no date
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
