26a182bda9bb524f735613409ba331c526ad8730
ajlaroya <arthur.j.laroya@gmail.com>
2021-09-25 10:46:36 +1000

    created Post and Comment models

--- blog_project/sari_sari/blog/models.py
+++ blog_project/sari_sari/blog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse # after action redirection
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE) # authorised user
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True,null=True)
+
+    # linked to a publish button
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+    def get_absolute_url(self): # django method to create URL for instance of post
+        return reverse('post_detail',kwargs={'pk':self.pk}) # goes poes_detail view with primary key
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post',related_name='comments',on_delete=models.CASCADE) # comment connected to post
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approved(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
