d679b7508b041673141b0719cee3154086242f9c
ajlaroya <arthur.j.laroya@gmail.com>
2021-11-05 22:25:18 +1100

    bulma tooltips, footer links

--- socialproject/posts/models.py
+++ socialproject/posts/models.py
@@ -13,6 +13,17 @@ class Post(models.Model):
     message_html = models.TextField(editable=False)
     group = models.ForeignKey(Group,related_name='posts',null=True,blank=True,on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, blank=True, related_name='likes')
+    #parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='+')
+
+    @property
+    def children(self):
+        return Comment.objects.filter(parent=self).order_by('-created_on').all()
+
+    @property
+    def is_parent(self):
+        if self.parent is None:
+            return True
+        return False
 
     def __str__(self):
         return self.message
