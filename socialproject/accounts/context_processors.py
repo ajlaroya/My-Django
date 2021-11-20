67507fec25374f3bea614b3cf7f480f2b0107d4c
ajlaroya <arthur.j.laroya@gmail.com>
2021-11-21 10:27:14 +1100

    avatars in dms, profile links in dms, ui fixes

--- socialproject/accounts/context_processors.py
+++ socialproject/accounts/context_processors.py
@@ -1,6 +1,7 @@
 from accounts.models import UserProfile
 
 def add_variable_to_context(request):
+    ''' Makes profile a global variable so I can access avatars from nav '''
     if request.user.is_authenticated:
         user = request.user
         profile = UserProfile.objects.get(pk=user.pk)
