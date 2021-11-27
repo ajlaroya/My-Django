31350ac0ff1ac4966be2c7d3ad2100f880b0ac8d
ajlaroya <arthur.j.laroya@gmail.com>
2021-11-27 19:02:29 +1100

    create thread and messaging templates cleanup, commented, refined

--- socialproject/accounts/forms.py
+++ socialproject/accounts/forms.py
@@ -8,8 +8,9 @@ class ThreadForm(forms.Form):
 
 class MessageForm(forms.ModelForm):
     ''' Form for the individual message '''
-    body = forms.CharField(label='', max_length=1000)
-    image = forms.ImageField(label='Send an Image', required=False)
+    body = forms.CharField(label='', max_length=1000,
+        widget=forms.TextInput(attrs={'placeholder':'Say something', 'class': 'is-warning is-rounded mb-5'}))
+    image = forms.ImageField(label='Send an Image', required=False,)
 
     class Meta:
         model = MessageModel
