d46da557272be142c2d01edbb88d5f55a8b14106
ajlaroya <arthur.j.laroya@gmail.com>
2021-11-29 21:38:52 +1100

    minor fixes, docstrings, group list ui changes, group detail title

--- socialproject/accounts/templatetags/custom_tags.py
+++ socialproject/accounts/templatetags/custom_tags.py
@@ -1,3 +1,10 @@
+'''
+Create custom logic to get notifications without creating another view.
+We can get users their context and can filter the notifications by user and
+if it has not been seen. These notifications are then returned in the context
+and can be accessed them from any
+'''
+
 from django import template
 from accounts.models import Notification
 
@@ -5,6 +12,9 @@ register = template.Library()
 
 @register.inclusion_tag('accounts/show_notifications.html', takes_context=True)
 def show_notifications(context):
+    '''Shows notifications to users and excludes seen notifcations and orders
+    by date. Notifications are returned via context to show_notifications.html'''
     request_user = context['request'].user
-    notifications = Notification.objects.filter(to_user=request_user).exclude(user_has_seen=True).order_by('-date')
+    notifications = Notification.objects.filter(to_user=request_user).exclude(
+    user_has_seen=True).order_by('-date')
     return {'notifications': notifications}
